42a63ef406af1adb9e20c2a9469bdb1cd6d0ab82
Abolfazl Sabagh <a.sabagh72@gmail.com>
2020-09-18 20:51:35 +0430

    get checkout fields

--- get-wc-checkout-fields.php
+++ get-wc-checkout-fields.php
@@ -1,4 +1,4 @@
 <?php
-$countries = new new WC_Countries();
+$countries = new WC_Countries();
 $billing_fields = $countries->get_address_fields( $countries->get_base_country(),'billing_');
 $shipping_fields = $countries->get_address_fields( $countries->get_base_country(),'shipping_');
